0fe981e36efbfa3aa49ecec8afae2aa32be26eb0
dudunwei <ddw99402@qq.com>
2024-09-06 23:17:25 +0800

    添加购物车 代码

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,26 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/user/shoppingCart")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/add")
+    public Result add(@RequestBody  ShoppingCartDTO shoppingCartDTO){
+        shoppingCartService.add(shoppingCartDTO);
+        return Result.success();
+    }
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,78 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Slf4j
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    public void add(ShoppingCartDTO shoppingCartDTO) {
+        // 判断当前加入的商品是否已经存在了
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        // 已经存在，+1
+        if (list != null && list.size()>0){
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber()+1);
+            shoppingCartMapper.updateNumberById(cart);
+        }else {
+            // 不存在，插入一条购物车数据
+            Long dishId = shoppingCartDTO.getDishId();
+            // 如果dishId不为空，说明增加的是菜品
+            if (dishId != null){
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+
+            }else {
+                // 增加的是套餐
+                Long setmealId = shoppingCart.getSetmealId();
+                Setmeal setmeal = setmealMapper.getById(setmealId);
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            // 插入
+            shoppingCartMapper.insert(shoppingCart);
+        }
+
+
+
+
+    }
+}
