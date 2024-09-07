d7ef91495a495d69090324cd5ac5dacc43c36ca1
dudunwei <ddw99402@qq.com>
2024-09-07 17:15:05 +0800

    跳过微信支付 代码

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,18 +1,22 @@
 package com.sky.controller.user;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController("userOrderController")
 @RequestMapping("/user/order")
+@Slf4j
 public class OrderController {
 
     @Autowired
@@ -25,5 +29,27 @@ public class OrderController {
 
         return Result.success(orderSubmitVO);
 
+    }
+
+    /**
+     * 跳过订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @PutMapping("/payment")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        log.info("订单支付：{}", ordersPaymentDTO);
+//        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+//        log.info("生成预支付交易单：{}", orderPaymentVO);
+        orderService.paySuccess(ordersPaymentDTO.getOrderNumber());
+
+        OrderPaymentVO vo = new OrderPaymentVO();
+        vo.setNonceStr("随机字符串");
+        vo.setPaySign("签名");
+        vo.setTimeStamp(LocalDateTime.now().toString());
+        vo.setPackageStr("prepay_id");
+
+        return Result.success(vo);
     }
 }
